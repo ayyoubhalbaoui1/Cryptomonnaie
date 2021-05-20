497d7142f6b24ca5b8d1089fd2869850fe38a6e1
ayyoubhalbaoui1 <ayyoubhalbaoui1@gmail.com>
2021-05-20 16:49:49 +0100

    Changes

--- configs/firebase.js
+++ configs/firebase.js
@@ -1,11 +1,11 @@
 import firebase from "firebase";
 var firebaseConfig = {
-  apiKey: "AIzaSyAKAk5WL9J8tGbNw3RgFcM8WEBGeXLtGGs",
-  authDomain: "cryprocurrency-74898.firebaseapp.com",
-  projectId: "cryprocurrency-74898",
-  storageBucket: "cryprocurrency-74898.appspot.com",
-  messagingSenderId: "194471126131",
-  appId: "1:194471126131:web:1fb6875a13edc6f7517606"
+  apiKey: "AIzaSyDbUurpcKsQoO2wUtuW7ogr2WX5piTRS0Q",
+    authDomain: "cryptotest-2a552.firebaseapp.com",
+    projectId: "cryptotest-2a552",
+    storageBucket: "cryptotest-2a552.appspot.com",
+    messagingSenderId: "901410321184",
+    appId: "1:901410321184:web:3b56b842d838c83645dd45"
 };
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
